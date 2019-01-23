2461e86129e3c1627a3e512af9b16ef088b4d08b
Fontkodo <alexgriffinsrq@gmail.com>
2019-01-23 14:38:10 -0500

    First complete rough draft

--- book_sentiment.R
+++ book_sentiment.R
@@ -8,6 +8,12 @@ library(tidytext)
 library(gutenbergr)
 
 book_sentiment <- function(id, seg_length) {
+  #id is the gutenberg_id of the desired document
+  #seg_length is the number of lines that are analyzed at a time
+  #This only works with text documents
+  #This returns a tibble which associates each segment of the book with
+  #a score based on the sentiment values of the words in the segment
+  #This uses the Bing lexicon for scoring
   return(
     gutenberg_download(id) %>%
       mutate(linenumber = row_number()) %>%
@@ -20,12 +26,16 @@ book_sentiment <- function(id, seg_length) {
 }
 
 book_sentiment_graph <- function(id, seg_length) {
+  #This creates a visual of the book_sentiment function
   book_sentiment(id, seg_length) %>%
     ggplot(aes(index, sentiment)) +
     geom_col()
 }
 
 book_sentiment_absolute <- function(id, seg_length) {
+  #This is similar to the book_sentiment function, but the score
+  #is, instead of the number of positives minus the number of negatives, the
+  #sum of the positive and negative counts
   return(
     gutenberg_download(id) %>%
       mutate(linenumber = row_number()) %>%
@@ -38,6 +48,7 @@ book_sentiment_absolute <- function(id, seg_length) {
 }
 
 book_sentiment_absolute_graph <- function(id, seg_length) {
+  #This produces a visual of the book_sentiment_absolute
   book_sentiment_absolute(id, seg_length) %>%
     ggplot(aes(index, sentiment)) +
     geom_col()
@@ -52,5 +63,17 @@ book_sentiment_intensity <- function(id, seg_length) {
       count(index = linenumber %/% seg_length, score) %>%
       spread(score, n, fill = 0) %>%
       mutate(intensity = ())
+  )
+}
+
+book_sentiment_ratio <- function(id, seg_length) {
+  temp_book <- gutenberg_download(id) %>%
+    mutate(linenumber = row_number()) %>%
+    unnest_tokens(word, text) %>%
+    left_join(get_sentiments("bing")) %>%
+    count(index = linenumber %/% seg_length, sentiment)
+  return(
+    sum((filter(temp_book, (sentiment == "positive")|(sentiment == "negative")))[, 3])
+    / sum(temp_book[, 3])
   )
 }
